c25ad0ac9b5ffee0a9a5925058f3b1ec603a01b2
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-24 17:54:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+function validateForm(event) {
+  // Prevent the form from submitting
+  event.preventDefault();
+
+  // Get form inputs
+  let name = document.getElementById("name").value;
+  let age = parseInt(document.getElementById("age").value);
+
+  // Validate inputs
+  if (name.trim() === "" || isNaN(age)) {
+    alert("inputs cannot be empty.");
+    return false;
+  }
+
+  // Create a promise
+  let formPromise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (age >= 18) {
+        resolve(name);
+      } else {
+        reject(name);
+      }
+    }, 4000);
+  });
+
+  // Handle promise resolution and rejection
+  formPromise
+    .then(function (name) {
+      alert("Welcome, " + name + ". You can vote.");
+    })
+    .catch(function (name) {
+      alert("Oh sorry " + name + ". You aren't old enough.");
+    });
+}
